844e31628c1559070d16d81be1d3f47fd450df94
Ardenn D. Jenar <106331743+Noctilucent139@users.noreply.github.com>
2024-01-09 21:01:51 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,20 @@
 
 <?php
+
 // Database connection parameters
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";  // should be changed if required 
+// $password = "root";
 $dbname = "online_library";
-$port = 8889;
+// $port = 8889;
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
 die("Connection failed: " . mysqli_connect_error());
 }
+    
 ?>
